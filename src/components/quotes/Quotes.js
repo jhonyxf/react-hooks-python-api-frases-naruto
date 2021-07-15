973c6bbaab9bf025820b3170ed66a104060956e9
sidney_silva_verity <jhonyxf@gmail.com>
2021-07-15 17:25:17 -0300

    Ajustes estéticos

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,9 +1,37 @@
-export const Quotes = ({ quote, speaker }) => {
+import styled from 'styled-components';
+import { string } from 'prop-types';
+import { Button } from '../../components';
+
+export const Quotes = ({ quote, speaker, onUpdate }) => {
   return(
-    <>
-    <p>{quote}</p>
-    <p>{speaker}</p>
-    <button>Aqui o botão</button>
-    </>
+    <Wrapper>
+    <Quote>{quote}</Quote>
+    <Speaker>- {speaker}</Speaker>
+    <Button onClick={onUpdate}>Aqui o botão</Button>
+    </Wrapper>
   )
-}
+};
+
+Quotes.propTypes = {
+  quote: string,
+  speaker: string,
+  onUpdate: func
+};
+
+const Wrapper = styled.div`
+  flex:1;
+  display:flex;
+  flex-direction: column;
+  align-items:center;
+`;
+
+const Quote = styled.p`
+  font-size: 2em;
+  flex:1;
+  margin: 0;
+`;
+
+const Speaker = styled(Quote)`
+text-align:right;
+margin-bottom: 5px
+`;
